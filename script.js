f9371ac709cabf78a495c3ab4ff53276233b186a
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2023-12-13 10:52:25 +0000

    delete

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-let submit = document.getElementById("submit");
+// let submit = document.getElementById("submit");
 let addItem = document.getElementById("addItem");
+// let items = document.getElementById("items")
 
-let items = document.getElementById("items")
+let ul = document.getElementById("ul");
+
+let li = document.createElement('li'); 
+
+let del = document.getElementById("del")
+
+// submit.addEventListener("click", function(){
+
+//     text =addItem.value
+//     console.log("hello", text)
+//     items.innerHTML = text
+
+//     addItem.value = "";
+
+ 
+//     ul.appendChild(newLi)
+
+
+// })
+
 
 submit.addEventListener("click", function(){
 
     text =addItem.value
-    console.log("hello", text)
-    items.innerHTML = text
+    // console.log("hello", text)
+    // items.innerHTML = text
+
+    // liText = document.createTextNode(text)
+
+    let newText = document.createTextNode(text) 
+
+    li.appendChild(newText)
+
+    ul.appendChild(li)
+
+
 
     addItem.value = "";
 
 
+
 })
+
+del.addEventListener("click", function(){
+    console.log("delete")
+    ul.removeChild(li)
+})
+
+
